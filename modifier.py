17dd1491b57811669887a48b42ea8461cb33450a
riizdo <ricardoizquierdopro@gmail.com>
2020-11-18 00:17:54 +0100

    commit actual modifier

--- modifier.py
+++ modifier.py
@@ -6,16 +6,21 @@ class Modifier():
     def __init__(self):
         self.__version = 0
         self.__header = 'is a program to simplify the modification of the motoman ladder, {} version'.format(self.__version)
+        self.__path = ''
         
         
     def __str__(self):
         return self.__header
     
     
-    
+    def begin(self):
+        print(self)
+        self.__path = input('enter the name of archive and his path: ')
+        
+        
     
     
     
 if __name__ == '__main__':
     app = Modifier()
-    print(app)
+    app.begin()
