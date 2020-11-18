690a8896b054178684b242164bb4e515be0eee7f
riizdo <ricardoizquierdopro@gmail.com>
2020-11-18 23:38:19 +0100

    commit continue work

--- modifier.py
+++ modifier.py
@@ -5,22 +5,44 @@
 class Modifier():
     def __init__(self):
         self.__version = 0
-        self.__header = 'is a program to simplify the modification of the motoman ladder, {} version'.format(self.__version)
+        self.__header = 'is a program to simplify the modification of the motoman ladder, version: {}.'.format(self.__version)
         self.__path = ''
+        self.__data = ''
+        
+        self.__begin()
         
         
     def __str__(self):
         return self.__header
     
     
-    def begin(self):
+    def __begin(self):
         print(self)
-        self.__path = input('enter the name of archive and his path: ')
+        self.__path = input('enter the name of archive: ')
+        
+        try:
+            f = open(self.__path, 'r')
+            f.close()
+            self.__readFile(self.__path)
+        except:
+            print('the file no exist or the path is incorrect')
+            
+            
+    def __menu():
+        pass
         
         
-    
+    def __readFile(self, file):
+        f = open(file, 'r')
+        self.__data = f.read()
+        f.close()
+        
+        
+    def printData(self):
+        print(self.__data)
+        
     
     
 if __name__ == '__main__':
     app = Modifier()
-    app.begin()
+    app.printData()
