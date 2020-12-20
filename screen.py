5e375673329ab0762ba66a19a7b691a51ecc432e
riizdo <ricardoizquierdopro@gmail.com>
2020-12-21 00:26:05 +0100

    develop screen.py

--- screen.py
+++ screen.py
@@ -9,23 +9,37 @@ class Screen(Frame):
         self.master.geometry('1200x600+0+0')
         self.master.title('Ladder Modifier')
         
-        self.__menuBar = Menu(self)
-        self.master.config(menu = self.__menuBar)
+        self.__createMenuBar()
         
-        self.__fileMenu = Menu(self.__menuBar, tearoff = 0)
-        self.__menuBar.add_cascade(label = 'Archivo', menu = self.__fileMenu)
-        self.__fileMenu.add_command(label = 'Abrir', command = self.__loadFile)
-        
-        self.__editMenu = Menu(self.__menuBar, tearoff = 0)
-        self.__menuBar.add_cascade(label = 'Editar', menu = self.__editMenu)
         
         self.p = Button(self, text = 'hola', width = 10)
         self.p.pack()
     
     
     def __loadFile(self):
-        file = askopenfilename(filetypes=(("Ladder files", "*.LST"),\
-                                              ("All files", "*.*") ))
+        file = askopenfilename(filetypes=(("Ladder files", "*.LST"), ("All files", "*.*") ))
+        
+        
+    def __saveFile(self):
+        pass
+    
+    
+    def __saveAs(self):
+        pass
+        
+        
+    def __createMenuBar(self):
+        self.__menuBar = Menu(self)
+        self.master.config(menu = self.__menuBar)
+        
+        self.__fileMenu = Menu(self.__menuBar, tearoff = 0)
+        self.__menuBar.add_cascade(label = 'Archivo', menu = self.__fileMenu)
+        self.__fileMenu.add_command(label = 'Abrir', command = self.__loadFile)
+        self.__fileMenu.add_command(label = 'Guardar', command = self.__saveFile)
+        self.__fileMenu.add_command(label = 'Guardar como..', command = self.__saveAs)
+        
+        self.__editMenu = Menu(self.__menuBar, tearoff = 0)
+        self.__menuBar.add_cascade(label = 'Editar', menu = self.__editMenu)
         
     
     def destroyElement(self, element):
