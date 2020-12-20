2e8cedc0d03b7864d924017d79446a73ca4310c1
riizdo <ricardoizquierdopro@gmail.com>
2020-12-20 19:15:05 +0100

    add app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from tkinter import *
+import screen
+import modifier
+
+
+class App():
+    def __init__(self):
+        root = Tk()
+        self.__screen = screen.Screen(root)
+        
+        
+        self.__screen.mainloop()
+        
+        
+if __name__ == '__main__':
+    app = App()
