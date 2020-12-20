6731b86f2e7ca7e5ecfa267b3b3ea54ed7847735
riizdo <ricardoizquierdopro@gmail.com>
2020-12-21 00:18:15 +0100

    develop screen.py

--- screen.py
+++ screen.py
@@ -6,22 +6,34 @@ class Screen(Frame):
     def __init__(self, master = None):
         Frame.__init__(self, master)
         self.pack()
-        self.master.geometry('900x600+0+0')
-        self.master.title('Proj')
+        self.master.geometry('1200x600+0+0')
+        self.master.title('Ladder Modifier')
         
-        self.p = Button(self, text = 'hola', command = self.loadFile, width = 10)
+        self.__menuBar = Menu(self)
+        self.master.config(menu = self.__menuBar)
+        
+        self.__fileMenu = Menu(self.__menuBar, tearoff = 0)
+        self.__menuBar.add_cascade(label = 'Archivo', menu = self.__fileMenu)
+        self.__fileMenu.add_command(label = 'Abrir', command = self.__loadFile)
+        
+        self.__editMenu = Menu(self.__menuBar, tearoff = 0)
+        self.__menuBar.add_cascade(label = 'Editar', menu = self.__editMenu)
+        
+        self.p = Button(self, text = 'hola', width = 10)
         self.p.pack()
-        
-        
-    def button(self, text, location = None):
-        pass
     
     
-    def loadFile(self):
-        file = askopenfilename(filetypes=(("Template files", "*.tplate"),\
-                                              ("HTML files", "*.html;*.htm"),\
+    def __loadFile(self):
+        file = askopenfilename(filetypes=(("Ladder files", "*.LST"),\
                                               ("All files", "*.*") ))
         
+    
+    def destroyElement(self, element):
+        element.destroy()
+        
+        
+    def main(self):
+        pass
         
 
 if __name__ == '__main__':
